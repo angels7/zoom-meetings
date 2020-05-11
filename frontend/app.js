1f92a27e23e1bad4e8239b02638fb9057934f511
Angel <wellangel7@gmail.com>
2020-05-11 13:41:05 +0200

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataFetching from './components/DataFetch'
+import PostForm from './components/PostForm'
+
+function App() {
+  return(
+    <div>
+      <DataFetching />
+    </div>
+  )
+}
+
+export default App;
